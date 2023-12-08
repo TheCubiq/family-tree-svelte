b76931f28aadfb0286d8754fc3227a35038c577c
TheCubiq <jlajsek@gmail.com>
2023-12-08 03:19:27 +0100

    💀 custom domain xd

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/family-tree-svelte/",
   plugins: [svelte()],
   server: {
     port: 6969,
